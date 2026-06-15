b548f0982a7d7e003431a39d790ed0b4b997e31f
YALLICO OWNER <yallico2024@gmail.com>
2026-06-15 10:35:37 -0500

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -119,7 +119,7 @@ let handler = async (m, { conn, usedPrefix }) => {
 handler.help = ['menu', 'menú']
 handler.tags = ['main']
 handler.command = ['menu', 'menú', 'help', 'ayuda']
-handler.register = true
+handler.register = false
 export default handler
 
 const clockString = ms =>
